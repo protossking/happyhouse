2a42f897f666b9beb9c60ea276053b1fda172645
upgrademarine5 <upgrademarine5@icloud.com>
2021-12-07 21:41:05 +0900

    commit

---
+++ happyhouse_final-2/src/main/java/com/happyhouse/HappyhouseFinal2Application.java
@@ -0,0 +1,13 @@
+package com.happyhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyhouseFinal2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyhouseFinal2Application.class, args);
+	}
+
+}
